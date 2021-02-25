5091d8ebf3fdc86bb941f45f0cde094c98d1d603
GitJumpers <gitjumpers@gmail.com>
2021-02-25 19:00:15 +0200

    Sibahle Mayekiso has created 2 files together with the project architecture

---
+++ DataAccessLayer/fileSystem.java
@@ -0,0 +1,10 @@
+//File created by Sibahle Mayekiso
+package DataAccessLayer;
+
+//Importing of Libraries
+import java.io.*;
+
+public class fileSystem {
+
+    
+}
---
+++ DataAccessLayer/sqlDBSytem.java
@@ -0,0 +1,10 @@
+//File created by Sibahle Mayekiso
+package DataAccessLayer;
+
+//Importing of Libraries
+import java.sql.*;
+
+
+public class sqlDBSytem {
+  
+}

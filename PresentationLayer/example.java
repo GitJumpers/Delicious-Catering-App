84b9bccf3e2743a4454ee71cd302b3759f4c3c3e
Your Name <576467@student.belgiumcampus.ac.za>
2021-02-26 12:47:41 +0200

    AskedQuestionsAndPrintedWords_Tiaan

--- PresentationLayer/example.java
+++ PresentationLayer/example.java
@@ -1,15 +1,29 @@
 package PresentationLayer;
-import java.io.IOException;
-import java.util.*;
-
+import java.util.Scanner;
+import java.util.Date;
 
 public class example {
+    
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Delicious-Catering online booking system"); 
+        System.out.println("---------------------------------------------------------"); 
+        System.out.println(" "); 
+        System.out.println("Please enter your first name"); 
+        String Fname = scanner.nextLine();
+        System.out.println("Please enter your last name"); 
+        String Lname = scanner.nextLine();
+        System.out.println(" ");
+        System.out.println("Hallo " + Fname + " " + Lname + ", what is the date of youre required catering service?");
         
-        System.out.println("Tiaan"); 
+        //get date from user
+
+        
+
+
+
+
+        scanner.close();
     }
-    
-    //TheLanie
-    //comment
-    
 }

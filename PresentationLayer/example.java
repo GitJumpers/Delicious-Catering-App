3fea11507f45156cc5458d11ff219abead68ecec
Your Name <576467@student.belgiumcampus.ac.za>
2021-02-27 11:32:36 +0200

    BasicStructure_Tiaan

--- PresentationLayer/example.java
+++ PresentationLayer/example.java
@@ -16,8 +16,32 @@ public class example {
         String Lname = scanner.nextLine();
         System.out.println(" ");
         System.out.println("Hallo " + Fname + " " + Lname + ", what is the date of youre required catering service?");
-        
+        System.out.println(" ");
+
         //get date from user
+
+        
+
+        //date is availeble or date is already taken
+
+        System.out.println("What kind of event will the catering service be required for");
+        System.out.println("Example.... Babtism or a Wedding");
+        String EventType = scanner.nextLine();
+        System.out.println("Will the " + EventType + " need any decorating");
+
+        String Answer = scanner.nextLine();
+        if (Answer == "Yes") {
+            
+            //Ask Questions about decoration spesifications
+            System.out.println(""); 
+
+        } else {
+            // System.out.println("OK"); 
+        }
+
+        
+        
+
 
         
 

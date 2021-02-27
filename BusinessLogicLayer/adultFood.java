547feb9044d932e5704e6358210202d671395fca
GitJumpers <gitjumpers@gmail.com>
2021-02-27 14:59:26 +0200

    Added more meal variables to food classes - Maxwell Aereboe

--- BusinessLogicLayer/adultFood.java
+++ BusinessLogicLayer/adultFood.java
@@ -4,4 +4,7 @@ public class adultFood extends food
 {
     int chickenMeals;
     int steakMeals;
+    int gammonRoastMeals;
+    int pastaMeals;
+    int soupMeals;
 }
--- BusinessLogicLayer/childFood.java
+++ BusinessLogicLayer/childFood.java
@@ -4,4 +4,7 @@ public class childFood {
     int hotDogMeals;
     int chickenburgerMeals;
     int beefBurgerMeals;
+    int fishChipsMeals;
+    int pizzaMeals;
+    int pieMeals;
 }

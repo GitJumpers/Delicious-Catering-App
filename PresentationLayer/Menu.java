f81b37cad090ec9fcecbd5c12d1550edc65feaaf
GitJumpers <gitjumpers@gmail.com>
2021-03-06 19:17:38 +0200

    Added DisplayMenu() to enum Menu - Sibahle

--- PresentationLayer/Menu.java
+++ PresentationLayer/Menu.java
@@ -12,6 +12,16 @@ public enum Menu {
     private Menu(String lbl) {
        this.label = lbl;
     }
-
+    //Method will be invoked and will print menu items which the user will wish to act upon
+    public void DisplayMenu() {
+        System.out.println("Main Menu");
+        System.out.println("=========================================================");
+        System.out.println(Menu.NEW.label);
+        System.out.println(Menu.EDIT.label);
+        System.out.println(Menu.CONFIRMATION.label);
+        System.out.println(Menu.PAYMENT.label);
+        System.out.println(Menu.EXIT.label);
+        System.out.println("=========================================================");
+    }
 }
 
